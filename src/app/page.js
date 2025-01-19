dc3b5577b8b403f18246f52b0a51a6223c1005cf
Hitigome <kawibrian1@gmail.com>
2025-01-18 17:29:06 -0800

    x

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+<<<<<<< HEAD
 import Chat from "/components/chat.jsx";
+=======
+>>>>>>> 6f16edf (create logo and title)
 
 export default function Home() {
   return (
