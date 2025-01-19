db7bac6abbec75c21989d4bf1d4c55916cd282fb
belw7898 <bella.wang.10@gmail.com>
2025-01-19 10:24:59 -0800

    create change background feature

--- app/page.js
+++ app/page.js
@@ -26,12 +26,30 @@ const Home = () => {
     setSubjects(subjects.filter(subject => subject.id !== id));
   }
 
+  const [bgClass, setBgClass] = useState(styles.bg1);
+
+  const changeBackground = (newBgClass) => {
+    setBgClass(newBgClass);
+  };
+
   return (
-    <div className={styles.page}>
+    <div className={`${styles.page} ${bgClass}`}>
       <div className={styles.sidebar}>
         <h2>Subjects</h2>
         <SidebarInput newSubject={newSubject} setNewSubject={setNewSubject} addSubject={addSubject}/>
         <Sidebar subjects={subjects} removeSubject={removeSubject}/>
+        <h2>Change Background</h2>
+        <div>
+          <button className="backgroundButtons" onClick={() => changeBackground(styles.bg1)}>
+            <img src="/background.jpeg" style={{height:'40px', width: '60px'}}/>
+          </button>
+          <button className="backgroundButtons" onClick={() => changeBackground(styles.bg2)}>
+            <img src="/background1.jpg" style={{height:'40px', width: '60px'}}/>
+          </button>
+          <button className="backgroundButtons"  onClick={() => changeBackground(styles.bg3)}>
+            <img src="/background2.jpg" style={{height:'40px', width: '60px'}}/>
+          </button>
+        </div>
       </div>
       <div className={styles.body}>
       <Header/>
