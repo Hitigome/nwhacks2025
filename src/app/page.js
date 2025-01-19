be0bb2d0d0aae0bf3dc7c2de8e4e8ae341a243c2
Hitigome <kawibrian1@gmail.com>
2025-01-18 16:49:27 -0800

    empty home page for testing purposes

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 <<<<<<< HEAD
+<<<<<<< HEAD
 import Chat from "/components/chat.jsx";
 =======
 >>>>>>> 6f16edf (create logo and title)
+=======
+import Textbox from "/components/textbox.jsx";
+import Chat from "/components/chat.jsx";
+>>>>>>> 5b71347 (empty home page for testing purposes)
 
 export default function Home() {
   return (
