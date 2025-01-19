0476b5431011a90ef1d62ede7dbb8535c55938fe
belw7898 <bella.wang.10@gmail.com>
2025-01-19 08:08:20 -0800

    create light / dark mode

--- components/textbox.jsx
+++ components/textbox.jsx
@@ -3,8 +3,11 @@ import styles from './textbox.module.css'
 function Textbox() {
     return(
         <div className={styles.textbox}>
-            <p className={styles.p}>Enter your notes here:</p>
-            <textarea className={styles.input} defaultValue=""/>
+            <div>
+                <p className={styles.p}>Enter your notes here:</p>
+                <textarea className={styles.input} defaultValue=""/>
+            </div>
+            <button className={styles.button}>Summarize</button>
         </div>
     );
 }
