64946d266b6dab86456912b4cd933d469b5e644b
Hitigome <kawibrian1@gmail.com>
2025-01-18 16:49:27 -0800

    empty home page for testing purposes

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Chat from "/components/chat.jsx";
 import Textbox from "/components/textbox.jsx";
+import Chat from "/components/chat.jsx";
 
 export default function Home() {
   return (
